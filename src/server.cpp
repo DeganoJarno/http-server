697bd2f149b7235114861ed9504bc7b7f162ad06
DeganoJarno <jarno.d@hotmail.ch>
2025-07-14 23:15:23 +0200

    Adds status 200 response.

--- src/server.cpp
+++ src/server.cpp
@@ -51,10 +51,15 @@ int main(int argc, char **argv) {
   
   std::cout << "Waiting for a client to connect...\n";
   
-  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  int clinet_socket = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
   std::cout << "Client connected\n";
   
+  std::string request_buffer = "HTTP/1.1 200 OK\r\n\r\n";
+
+  send(clinet_socket, request_buffer, request_buffer.length, 0);
+
   close(server_fd);
+  close(clinet_socket);
 
   return 0;
 }
